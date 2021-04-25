e8b6c5ddccfb6d02c1642de9cafa5053ac594c32
Andres Valderrama <8341741+andresvalderrama@users.noreply.github.com>
2021-04-24 19:19:11 -0500

    Adding _snowpack folder

--- src/components/welcome/index.js
+++ src/components/welcome/index.js
@@ -1,4 +1,4 @@
-import { html as h } from "../../../pkg/htm/preact.js";
+import { html as h } from "../../../_snowpack/pkg/htm/preact.js.js";
 export default function Welcome(props) {
   return h`<div>Hello ${props.name}!</div>`;
 }
