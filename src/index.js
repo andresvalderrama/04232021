4f43b26fa40e546ffa2031c48e0654e519a39e7f
Andres Valderrama <8341741+andresvalderrama@users.noreply.github.com>
2021-04-24 19:56:31 -0500

    Removing underscore _ on fork

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { html } from "../pkg/htm/preact.js";
 import { render } from "../pkg/preact.js";
-import { prect } from "../_fork/prect.js";
+import { prect } from "../fork/prect.js";
 import Welcome from "./components/welcome/index.js";
 
 prect();
