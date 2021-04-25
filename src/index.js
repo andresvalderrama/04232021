3a90f276b38e7f47feb56c7411afe299fa90df37
Andres Valderrama <8341741+andresvalderrama@users.noreply.github.com>
2021-04-24 19:52:31 -0500

    Removing nested folder assets

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { html } from "../pkg/htm/preact.js";
 import { render } from "../pkg/preact.js";
-import { prect } from "../_fork/assets/prect.js";
+import { prect } from "../_fork/prect.js";
 import Welcome from "./components/welcome/index.js";
 
 prect();
