eb7f8ce5d71ffc7e36c0bd377a2f35ea852eca33
Andres Valderrama <8341741+andresvalderrama@users.noreply.github.com>
2021-04-24 19:11:25 -0500

    Test site deploy

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import "../_snowpack/pkg/preact/devtools.js";
-import {html} from "../_snowpack/pkg/htm/preact.js";
-import {render} from "../_snowpack/pkg/preact.js";
+import "../pkg/preact/devtools.js";
+import { html } from "../pkg/htm/preact.js";
+import { render } from "../pkg/preact.js";
 import Welcome from "./components/welcome/index.js";
 render(html`<${Welcome} name="World" />`, document.getElementById("root"));
